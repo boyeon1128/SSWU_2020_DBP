b03f4f80af52219ad993b2f125fc333570bf8a4a
Kim BoYeon <bylovem@naver.com>
2020-09-11 12:05:15 +0900

    W02-P

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+  $link = mysqli_connect('localhost:3307', 'root', '12345678', 'dbp');
+  $query = "SELECT * FROM playlist";
+  $result = mysqli_query($link, $query);
+  $list ='';
+  while($row = mysqli_fetch_array($result)) {
+    $list = $list."<li><a href=\"index.php?id={$row['id']}\">{$row['song_title']}</a></li>";
+  }
+
+$article = array (
+  'artist' => 'Welcome to my playlist',
+  'song_title' => 'song is ...'
+);
+
+if(isset($_GET['id'])) {
+  $query = "SELECT * FROM playlist WHERE id={$_GET['id']}";
+  $result = mysqli_query($link, $query);
+  $row = mysqli_fetch_array($result);
+  $article = array(
+    'artist' => $row['artist'],
+    'song_title' => $row['song_title']
+  );
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> PLAYLIST </title>
+</head>
+<body>
+  <h1><a href="index.php">PLAYLIST</a></h1>
+  <ol> <?= $list ?> </ol>
+  <a href="create.php">create</a>
+  <h2> <?= $article['artist'] ?> </h2>
+  <?= $article['song_title'] ?>
+</body>
+</html>
---
+++ process_create.php
@@ -0,0 +1,21 @@
+<?php
+  $link = mysqli_connect("localhost:3307", "root", "12345678", "dbp");
+
+  $query = "
+    INSERT INTO playlist (
+      artist, song_title, created
+      ) VALUE (
+        '{$_POST['artist']}',
+        '{$_POST['song_title']}',
+        now()
+        )
+  ";
+
+  $result = mysqli_query($link, $query);
+  if($result == false) {
+    echo '저장하는 과정에서 문제가 발생했습니다. 관리자에게 문의하세요.';
+    error_log(mysql_error($link));
+  } else {
+    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+  }
+?>
